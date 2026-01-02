c1399cb3f51971f98284538c751fcfec81413465
timtwice <hello@timilehin.com>
2026-01-02 22:59:44 +0000

    feat: add lc-0961 n-repeated-element-in-size-2n-array.

---
+++ lc_0961_n_repeated_element_in_size_2n_array.py
@@ -0,0 +1,41 @@
+"""
+LeetCode 0961 – N-Repeated Element in Size 2N Array
+Difficulty: Easy
+Link: https://leetcode.com/problems/n-repeated-element-in-size-2n-array/
+Topic: Array, Hash Table
+"""
+
+from typing import List
+
+"""
+Initial solution.
+"""
+class Solution:
+    def repeatedNTimes(self, nums: List[int]) -> int:
+        n = len(nums) // 2
+        lookup = {}
+
+        for _, num in enumerate(nums):
+            if num in lookup:
+                occurrence = lookup[num]
+                new_occurrence = occurrence + 1
+                
+                if new_occurrence == n: return num
+
+                lookup[num] = new_occurrence
+            else: 
+                lookup[num] = 1
+
+"""
+We can further optimise the above since there is a constraint that the array contains n+1 unique elements, so there won't be any duplicates except the element that is repeated n times.
+"""
+
+class Solution:
+    def repeatedNTimes(self, nums: List[int]) -> int:
+        lookup = set()
+        
+        for num in nums:
+            if num in lookup:
+                return num
+
+            lookup.add(num)
