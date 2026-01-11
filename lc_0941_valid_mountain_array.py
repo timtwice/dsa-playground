658c1ad83b48afdf7db4b50add243d5fe30925b9
timtwice <hello@timilehin.com>
2026-01-11 21:28:36 +0100

    chore: update lc_0941 valid_mountain_array

--- lc_0941_valid_mountain_array.py
+++ lc_0941_valid_mountain_array.py
@@ -36,3 +36,22 @@ class Solution:
             i += 1
                     
         return valid_ascent and valid_descent
+
+
+class Solution:
+    def validMountainArray(self, arr: List[int]) -> bool:
+        n = len(arr)
+        if n < 3:
+            return False
+
+        i = 0
+        while i + 1 < n and arr[i] < arr[i + 1]:
+            i += 1
+
+        if i == 0 or i == n - 1:
+            return False
+
+        while i + 1 < n and arr[i] > arr[i + 1]:
+            i += 1
+
+        return i == n - 1
