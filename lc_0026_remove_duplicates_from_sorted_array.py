f5978def0f04b3092a7d094285445fb129b8e581
timtwice <hello@timilehin.com>
2026-01-13 21:38:47 +0100

    refactor: update lc_0026 swapped comparison lhs and rhs

--- lc_0026_remove_duplicates_from_sorted_array.py
+++ lc_0026_remove_duplicates_from_sorted_array.py
@@ -10,7 +10,7 @@ class Solution:
         
         i = 1
         for j in range(1, len(nums)):
-            if nums[j] != nums[i - 1]:
+            if nums[i - 1] != nums[j]:
                 nums[i] = nums[j]
                 i += 1
         return i
