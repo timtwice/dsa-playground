7ba42ed4a442dddcbde1ee477633e716adfb3862
timtwice <hello@timilehin.com>
2026-01-14 22:23:13 +0100

    refactor: optimise logic for lc_0283 move_zeroes

--- lc_0283_move_zeroes.py
+++ lc_0283_move_zeroes.py
@@ -22,4 +22,22 @@ class Solution:
         
         for i in range(k, n):
             nums[i] = 0     
-           
+
+
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        
+        n = len(nums)
+        k = 0
+        
+        for i in range(n):
+            if nums[i] != 0:
+                nums[k], nums[i] = nums[i], nums[k]
+                k += 1
+        
+        
+                
+            
