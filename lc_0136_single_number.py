b07fd04659ec11cbbfd9dd9d7b27cafbacacc97d
Timilehin Timothy <hello@timilehin.com>
2026-01-25 23:55:26 +0100

    refactor: optimise logic for lc_0136 single_number

--- lc_0136_single_number.py
+++ lc_0136_single_number.py
@@ -17,4 +17,13 @@ class Solution:
                 lookup.add(num)
 
         return lookup.pop()
-        
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+
+        single_number = 0
+
+        for num in nums:
+            single_number ^= num
+
+        return single_number       
