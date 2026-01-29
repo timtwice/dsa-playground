79e1adf2fe8e8b3a375671f17d69e088681d1ad9
Timilehin Timothy <hello@timilehin.com>
2026-01-29 23:10:27 +0100

    refactor: optimise logic for lc_0268 missing_number

--- lc_0268_missing_number.py
+++ lc_0268_missing_number.py
@@ -17,4 +17,19 @@ class Solution:
                 missing = i
                 return missing
 
-        
+# Optimised approach - space O(1) and memory O(n)
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+
+        n = len(nums)
+        missing_sum = 0
+        actual_sum = 0
+
+        for i in range(n + 1):
+            if i < n:
+                missing_sum += nums[i]
+            actual_sum += i
+
+        return actual_sum - missing_sum
+
+             
