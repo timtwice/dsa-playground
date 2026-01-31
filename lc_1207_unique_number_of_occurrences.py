48908841f35b2ed1c0caa35d1a2b95a7ddd11e33
Timilehin Timothy <hello@timilehin.com>
2026-01-31 22:27:55 +0100

    feat: add lc_1207 unique_number_of_occurrences

---
+++ lc_1207_unique_number_of_occurrences.py
@@ -0,0 +1,27 @@
+"""
+LeetCode 1207 – Unique Number of Occurrences
+Difficulty: Easy
+Link: https://leetcode.com/problems/unique-number-of-occurrences/
+Topic: Array, Hash Table
+"""
+
+class Solution:
+    def uniqueOccurrences(self, arr: List[int]) -> bool:
+
+        lookup = {}
+
+        for num in arr:
+            if num in lookup:
+                lookup[num] += 1
+            else:
+                lookup[num] = 1
+
+        seen = set()
+
+        for val in lookup.values():
+            if val in seen:
+                return False
+            seen.add(val)
+        
+        return True
+        
