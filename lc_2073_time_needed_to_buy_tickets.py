ece862f3d902905aafba499682b66b5943de9531
Timilehin Timothy <hello@timilehin.com>
2026-02-03 23:04:28 +0100

    feat: add lc_2073 time_needed_to_buy_tickets

---
+++ lc_2073_time_needed_to_buy_tickets.py
@@ -0,0 +1,32 @@
+"""
+LeetCode 2073 – Time Needed to Buy Tickets
+Difficulty: Easy
+Link: https://leetcode.com/problems/time-needed-to-buy-tickets/
+Topic: Mid Level, Array, Queue, Simulator
+"""
+
+class Solution:
+    def timeRequiredToBuy(self, tickets: List[int], k: int) -> int:
+
+        count = tickets[k]
+        time = 0
+        i = 0
+        n = len(tickets)
+
+        while count > 0:
+            # if we have reached the end, reset to zero
+            if i == n:
+                i = 0
+            
+            if tickets[i] > 0:
+                tickets[i] -= 1
+                time += 1
+
+                if i == k:
+                    count -= 1
+            
+            i += 1
+
+        return time
+
+        
