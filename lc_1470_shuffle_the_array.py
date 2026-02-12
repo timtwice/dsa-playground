d7da1c10a1dfaaea469fa11c54ebe0bc7a639438
Timilehin Timothy <hello@timilehin.com>
2026-02-12 23:08:18 +0100

    feat: add lc_1470 shuffle_the_array

---
+++ lc_1470_shuffle_the_array.py
@@ -0,0 +1,24 @@
+"""
+LeetCode 1470 – Shuffle the Array
+Difficulty: Easy
+Link: https://leetcode.com/problems/shuffle-the-array/
+Topic: Mid Level, Array
+"""
+
+class Solution:
+    def shuffle(self, nums: List[int], n: int) -> List[int]:
+
+        shuffled = []
+
+        x = 0
+        y = n
+
+        while x < y and y < len(nums):
+            shuffled.append(nums[x])
+            shuffled.append(nums[y])
+
+            x += 1
+            y += 1
+        
+        return shuffled
+        
