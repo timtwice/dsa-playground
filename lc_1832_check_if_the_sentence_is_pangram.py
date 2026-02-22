9bc9c57b4d3f44197ad4c46c149a0f863258e2f1
Timilehin Timothy <hello@timilehin.com>
2026-02-22 22:53:20 +0100

    feat: add lc_1832 check_if_the_sentence_is_pangram

---
+++ lc_1832_check_if_the_sentence_is_pangram.py
@@ -0,0 +1,18 @@
+"""
+LeetCode 1832 – Check if the Sentence Is Pangram
+Difficulty: Easy
+Link: https://leetcode.com/problems/check-if-the-sentence-is-pangram/
+Topic: Mid Level, Hash Table, String
+"""
+
+class Solution:
+    def checkIfPangram(self, sentence: str) -> bool:
+
+        found = set()
+
+        for char in sentence:
+            if char not in found:
+                found.add(char)
+        
+        return len(found) == 26
+        
