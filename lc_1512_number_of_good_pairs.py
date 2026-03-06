602086a9fd86a2f8ea7227112fb936111cc0cbed
Timilehin Timothy <hello@timilehin.com>
2026-03-06 23:27:08 +0100

    refactor: trying to optimise logic for lc_1512 number_of_good_pairs

--- lc_1512_number_of_good_pairs.py
+++ lc_1512_number_of_good_pairs.py
@@ -18,3 +18,15 @@ class Solution:
             pairs += (count * (count - 1)) // 2
 
         return pairs
+
+class Solution:
+    def numIdenticalPairs(self, nums: List[int]) -> int:
+        
+        freq = {}
+        pairs = 0
+
+        for num in nums:
+            pairs += freq.get(num, 0)
+            freq[num] = freq.get(num, 0) + 1
+
+        return pairs
