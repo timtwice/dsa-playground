56a7468339092d4d9d1a052692dfbd2de8dcecd2
Timilehin Timothy <hello@timilehin.com>
2026-03-11 23:33:28 +0100

    feat: add lc_349 intersection_of_two_arrays

---
+++ lc_349_intersection_of_two_arrays.py
@@ -0,0 +1,22 @@
+"""
+LeetCode 349 – Intersection of Two Arrays
+Difficulty: Easy
+Link: https://leetcode.com/problems/intersection-of-two-arrays/
+Topic: Array, Hash Table, Two Pointers, Binary Search, Sorting
+"""
+
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+
+        set1 = set(nums1)
+        set2 = set(nums2)
+
+        intersection = []
+
+        for num in set1:
+            if num in set2:
+                intersection.append(num)
+
+        return intersection
+
+        
