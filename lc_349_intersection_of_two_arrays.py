bb9e069e88aaa9c0035044d8bc47fd6fd3b5e61a
Timilehin Timothy <hello@timilehin.com>
2026-03-12 23:05:39 +0100

    refactor: oneliner for lc_349 intersection_of_two_arrays

--- lc_349_intersection_of_two_arrays.py
+++ lc_349_intersection_of_two_arrays.py
@@ -19,4 +19,8 @@ class Solution:
 
         return intersection
 
-        
+       
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+
+        return list(set(nums1) & set(nums2))
