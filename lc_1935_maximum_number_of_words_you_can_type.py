8893a09f424ae27a866f4222a6a867d6af630c8c
Timilehin Timothy <hello@timilehin.com>
2026-03-14 22:45:34 +0100

    refactor: oneliner for lc_1935 maximum_number_of_words_you_can_type

--- lc_1935_maximum_number_of_words_you_can_type.py
+++ lc_1935_maximum_number_of_words_you_can_type.py
@@ -18,4 +18,11 @@ class Solution:
         return typed
 
 
+class Solution:
+    def canBeTypedWords(self, text: str, brokenLetters: str) -> int:
+
+        broken = set(brokenLetters)
+        return sum(all(ch not in broken for ch in word) for word in text.split())
+
+
         
