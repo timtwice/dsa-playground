651b13c60d0bd63c3ea219912a3b0ee5a9f1fe79
Timilehin Timothy <hello@timilehin.com>
2026-03-16 23:41:30 +0100

    feat: add lc_1929 concatenation_of_array

---
+++ lc_1929_concatenation_of_array.py
@@ -0,0 +1,20 @@
+"""
+LeetCode 1929 – Concatenation of Array
+Difficulty: Easy
+Link: https://leetcode.com/problems/concatenation-of-array/
+Topic: Mid Level, Array, Simulation
+"""
+
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+
+        n = len(nums)
+        n2 = 2 * n
+        ans = [0] * n2
+
+        for i, num in enumerate(nums):
+            ans[i] = num
+            ans[i + n] = num
+        
+        return ans
+        
