613da421a4ccce7c3be86093109f14d2a4e54f28
Timilehin Timothy <hello@timilehin.com>
2026-03-23 23:40:50 +0100

    feat: add lc_2235 add_two_integers

---
+++ lc_2235_add_two_integers.py
@@ -0,0 +1,11 @@
+"""
+LeetCode 2235 – Add Two Integers
+Difficulty: Easy
+Link: https://leetcode.com/problems/add-two-integers/
+Topic: Math
+"""
+
+class Solution:
+    def sum(self, num1: int, num2: int) -> int:
+        return num1 + num2
+        
