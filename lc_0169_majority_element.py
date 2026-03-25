f22db2adb9fd82ef9cd0fd012d692899d0ab1ff4
Timilehin Timothy <hello@timilehin.com>
2026-03-25 23:44:47 +0100

    refactor: optimise logic for lc_0169 majority_element

--- lc_0169_majority_element.py
+++ lc_0169_majority_element.py
@@ -21,3 +21,18 @@ class Solution:
             if lookup[num] > threshold:
                 return num
         
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+
+        majority = None
+        count = 0
+
+        for num in nums:
+            if count == 0:
+                majority = num
+            
+            count += 1 if num == majority else -1
+
+        return majority
+        
