cdcb7488e28b28787b04adf4d0da5f31e942f41f
Timilehin Timothy <hello@timilehin.com>
2026-03-26 21:25:07 +0100

    feat: add lc_0217 contains_duplicate

---
+++ lc_0217_contains_duplicate.py
@@ -0,0 +1,17 @@
+"""
+LeetCode 0217 – Contains Duplicate
+Difficulty: Easy
+Link: https://leetcode.com/problems/contains-duplicate/
+Topic: Array, Hash Table, Sorting
+"""
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        seen = set()
+
+        for num in nums:
+            if num in seen:
+                return True
+            seen.add(num)
+
+        return False
