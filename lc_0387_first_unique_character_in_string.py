5560ec4ad96ae04e84cd4afa472a10cf788e4076
Timilehin Timothy <hello@timilehin.com>
2026-03-29 23:46:02 +0200

    refactor: use Counter class, for lc_0387 first_unique_character_in_string

--- lc_0387_first_unique_character_in_string.py
+++ lc_0387_first_unique_character_in_string.py
@@ -20,3 +20,14 @@ class Solution:
         return -1
 
         
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+
+        counts = Counter(s)
+
+        for i, ch in enumerate(s):
+            if counts[ch] == 1:
+                return i
+        return -1
+
+        
