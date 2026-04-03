69eb6b028c15406aee6475e358a0c24b8db63907
Timilehin Timothy <hello@timilehin.com>
2026-04-03 23:52:10 +0200

    refactor: O(1), for lc_0645 lc_0645 set_mismatch

--- lc_0645_set_mismatch.py
+++ lc_0645_set_mismatch.py
@@ -19,4 +19,23 @@ class Solution:
                 missing = i
 
         return [dup, missing]
+
+class Solution:
+    def findErrorNums(self, nums: List[int]) -> List[int]:
+        dup = 0
+        missing = 0
+
+        for num in nums:
+            idx = abs(num) - 1
+            if nums[idx] < 0:
+                dup = abs(num)
+            else:
+                nums[idx] *= -1
+        
+        for i in range(len(nums)):
+            if nums[i] > 0:
+                missing = i + 1
+        
+        return [dup, missing]
+
         
