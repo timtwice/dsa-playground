1098b420077be447b7b5ca6550e03ac41caaf259
Timilehin Timothy <hello@timilehin.com>
2026-04-05 23:47:29 +0200

    refactor: oneliner, for lc_0645 lc_1748 sum_of_unique_elements

--- lc_1748_sum_of_unique_elements.py
+++ lc_1748_sum_of_unique_elements.py
@@ -16,4 +16,9 @@ class Solution:
         
         return unique_sum
         
+
+class Solution:
+    def sumOfUnique(self, nums: List[int]) -> int:
+        return sum(num for num, count in Counter(nums).items() if count == 1)
+        
         
