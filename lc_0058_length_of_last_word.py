0c325dc3e460a04f170bd89509425a8495dcaf37
Timilehin Timothy <hello@timilehin.com>
2026-04-07 23:46:33 +0200

    feat: add lc_0058 length_of_last_word

---
+++ lc_0058_length_of_last_word.py
@@ -0,0 +1,18 @@
+"""
+LeetCode 0058 – Length of Last Word
+Difficulty: Easy
+Link: https://leetcode.com/problems/length-of-last-word/
+Topic: String
+"""
+
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+
+        last_word = ''
+
+        for word in s.split(' '):
+            if word.strip():
+                last_word = word
+        
+        return len(last_word)
+        
