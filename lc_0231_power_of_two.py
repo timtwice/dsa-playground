ef57a87c8d864c3ddd917d688e3ba21fb555c82a
Timilehin Timothy <hello@timilehin.com>
2026-04-10 23:31:36 +0200

    refactor: recursion implementation, for lc_0645 lc_0231 power_of_two

--- lc_0231_power_of_two.py
+++ lc_0231_power_of_two.py
@@ -20,3 +20,13 @@ class Solution:
         return True
 
         
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+
+        if n <= 0:
+            return False
+        if n == 1:
+            return True
+        if n % 2 != 0:
+            return False
+        return self.isPowerOfTwo(n // 2)
