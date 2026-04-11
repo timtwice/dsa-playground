ea46bf3d44aa434c062e8fe0eaf0c03d12d9cfb0
Timilehin Timothy <hello@timilehin.com>
2026-04-11 23:53:26 +0200

    feat: add lc_0258 add_digits

---
+++ lc_0258_add_digits.py
@@ -0,0 +1,19 @@
+"""
+LeetCode 0258 – Add Digits
+Difficulty: Easy
+Link: https://leetcode.com/problems/add-digits/
+Topic: Math, Simulation, Number Theory
+"""
+
+class Solution:
+    def addDigits(self, num: int) -> int:
+        
+        while num >= 10:
+            total = 0
+            while num > 0:
+                total += num % 10
+                num //= 10
+            num = total
+        return num
+
+        
