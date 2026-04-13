7ab1ad5d086cd0cc316426dd15c728ce0b7a733d
Timilehin Timothy <hello@timilehin.com>
2026-04-13 23:43:53 +0200

    feat: add lc_0344 reverse_string

---
+++ lc_0344_reverse_string.py
@@ -0,0 +1,19 @@
+"""
+LeetCode 0344 – Reverse String
+Difficulty: Easy
+Link: https://leetcode.com/problems/reverse-string/
+Topic: Two Pointers, String
+"""
+
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        l = 0
+        r = len(s) - 1
+
+        while l < r:
+            s[l], s[r] = s[r], s[l]
+            l += 1
+            r -= 1
